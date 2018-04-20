5124e0d3003fe494b9fb2512bf5960c4a4b6e7f7
Paul Rubenstein <pruben@login2.is.localnet>
2018-04-20 12:16:48 +0200

    added saving and loading
    Signed-off-by: Paul Rubenstein <pruben@login2.is.localnet>

--- factor_vae.py
+++ factor_vae.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-
+import sys
 
 class FactorVAE(object):
     def __init__(self):
@@ -13,34 +13,49 @@ class FactorVAE(object):
         self.ae_train_step, self.disc_train_step = self._optimizer_init()
 
         self.sess = tf.Session()
+        self.saver = tf.train.Saver(keep_checkpoint_every_n_hours=2)
         self.sess.run(tf.global_variables_initializer())
 
-        self.train()
 
     def train(self):
         print("Beginning training")
         it=0
         while it < 300000:
-            sess.run(self.ae_train_step, {self.input_ph: sample_minibatch()})
-            sess.run(self.ae_train_step, {self.input_ph: sample_minibatch()})
+            it += 1
+            self.sess.run(self.ae_train_step, {self.input_ph: self.sample_minibatch()})
+            self.sess.run(self.disc_train_step, {self.input_ph: self.sample_minibatch()})
 
             if it % 100 == 0:
-                batch = sample_minibatch()
-                ae_train_loss = sess.run(self.auto_encoder_loss, {self.input_ph: batch})
-                recon_train_loss = sess.run(self.recon_loss, {self.input_ph: batch})
-                disc_train_loss = sess.run(self.disc_loss, {self.input_ph: batch})
-                print("Iteration %i: \n    Autoencoder loss (train) %d\n    Reconstruction loss (train) %s\n    Discriminator loss (train) %s" % (it, ae_train_loss, recon_train_loss, disc_train_loss), flush=True)
+                batch = self.sample_minibatch()
+                ae_train_loss = self.sess.run(self.auto_encoder_loss, {self.input_ph: batch})
+                recon_train_loss = self.sess.run(self.recon_loss, {self.input_ph: batch})
+                disc_train_loss = self.sess.run(self.disc_loss, {self.input_ph: batch})
+                print("Iteration %i: \n    Autoencoder loss (train) %f\n    Reconstruction loss (train) %f\n    Discriminator loss (train) %f" % (it, ae_train_loss, recon_train_loss, disc_train_loss), flush=True)
 
-                ae_test_loss = sess.run(self.auto_encoder_loss, {self.input_ph: self.data_test[0:500]})
-                recon_test_loss = sess.run(self.recon_loss, {self.input_ph: self.data_test[0:500]})
-                disc_test_loss = sess.run(self.disc_loss, {self.input_ph: self.data_test[0:500]})
-                print("    Autoencoder loss (test) %d\n    Reconstruction loss (test) %s\n    Discriminator loss (test) %s" % (it, ae_test_loss, recon_test_loss, disc_test_loss), flush=True)
+                ae_test_loss = self.sess.run(self.auto_encoder_loss, {self.input_ph: self.data_test[0:500]})
+                recon_test_loss = self.sess.run(self.recon_loss, {self.input_ph: self.data_test[0:500]})
+                disc_test_loss = self.sess.run(self.disc_loss, {self.input_ph: self.data_test[0:500]})
+                print("    Autoencoder loss (test) %f\n    Reconstruction loss (test) %f\n    Discriminator loss (test) %f" % (ae_test_loss, recon_test_loss, disc_test_loss), flush=True)
 
+            if it % 100000 == 0:
+                model_path = "checkpoints/model"
+                save_path = self.saver.save(self.sess, model_path, global_step=it)
+                print("Model saved to: %s" % save_path)
 
-    def sample_minibatch(self, batch_size=64):
-        indices = np.random.choice(range(len(self.data_train)), batch_size, replace=False)
-        sample = self.data_train[indices]
+    def load_latest_checkpoint(self):
+        self.saver.restore(self.sess, tf.train.latest_checkpoint('checkpoints'))
+
+    def sample_minibatch(self, batch_size=64, test=False):
+        if test is False:
+            indices = np.random.choice(range(len(self.data_train)), batch_size, replace=False)
+            sample = self.data_train[indices]
+        elif test is True:
+            indices = np.random.choice(range(len(self.data_test)), batch_size, replace=False)
+            sample = self.data_test[indices]
         return sample
+
+    def make_plots(self):
+        pass
 
     def _data_init(self):
         # dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz must be in the root
@@ -52,8 +67,8 @@ class FactorVAE(object):
         # 90% random test/train split
         n_data = len(imgs)
         np.random.shuffle(imgs)
-        data_train = data[0 : (9*n_data)//10]
-        data_test = data[(9*n_data)//10 : ]
+        data_train = imgs[0 : (9*n_data)//10]
+        data_test = imgs[(9*n_data)//10 : ]
 
         return data_train, data_test
 
@@ -74,26 +89,44 @@ class FactorVAE(object):
 
     def _encoder_init(self, inputs):
         with tf.variable_scope("encoder"):
-            e_1 = tf.layers.conv2d(inputs=inputs, filters=32, kernel_size=4, stride=2, activation=tf.nn.relu, name="e_1")
-            e_2 = tf.layers.conv2d(inputs=e_1, filters=32, kernel_size=4, stride=2, activation=tf.nn.relu, name="e_2")
-            e_3 = tf.layers.conv2d(inputs=e_2, filters=64, kernel_size=4, stride=2, activation=tf.nn.relu, name="e_3")
-            e_4 = tf.layers.conv2d(inputs=e_3, filters=64, kernel_size=4, stride=2, activation=tf.nn.relu, name="e_4")
-            e_5 = tf.layers.dense(inputs=e_4, units=128, name="e_5")
+            e_1 = tf.layers.conv2d(inputs=inputs, filters=32, kernel_size=4, strides=2, activation=tf.nn.relu, name="e_1")
+            e_2 = tf.layers.conv2d(inputs=e_1, filters=32, kernel_size=4, strides=2, activation=tf.nn.relu, name="e_2")
+            e_3 = tf.layers.conv2d(inputs=e_2, filters=64, kernel_size=4, strides=2, activation=tf.nn.relu, name="e_3")
+            e_4 = tf.layers.conv2d(inputs=e_3, filters=64, kernel_size=4, strides=2, activation=tf.nn.relu, name="e_4")
+            e_4_reshape = tf.reshape(e_4, shape=[-1] + [np.prod(e_4.get_shape().as_list()[1:])])
+            e_5 = tf.layers.dense(inputs=e_4_reshape, units=128, name="e_5")
             e_mean = tf.layers.dense(inputs=e_5, units=self.z_dim, name="e_mean")
             e_logvar = tf.layers.dense(inputs=e_5, units=self.z_dim, name="e_logvar")
 
-        return input_ph, e_mean, e_logvar
+        return e_mean, e_logvar
 
     def _decoder_init(self, inputs, reuse=False):
         with tf.variable_scope("decoder"):
             d_1 = tf.layers.dense(inputs=inputs, units=128, activation=tf.nn.relu, name="d_1", reuse=reuse)
-            d_2 = tf.layers.dense(inputs=d_2, units=1024, activation=tf.nn.relu, name="d_2", reuse=reuse)
+            d_2 = tf.layers.dense(inputs=d_1, units=1024, activation=tf.nn.relu, name="d_2", reuse=reuse)
             d_2_reshape = tf.reshape(d_2, shape=[-1, 4, 4, 64])
-            d_3 = tf.layers.conv2d_transpose(inputs=d_2_reshape, filters=64, kernel_size=4, stride=2, activation=tf.nn.relu, name="d_3", reuse=reuse)
-            d_4 = tf.layers.conv2d_transpose(inputs=d_3, filters=32, kernel_size=4, stride=2, activation=tf.nn.relu, name="d_4", reuse=reuse)
-            d_out = tf.layers.conv2d_transpose(inputs=d_4, filters=1, kernel_size=4, stride=2, name="d_out", reuse=reuse)
-
+            d_3a = tf.layers.conv2d_transpose(inputs=d_2_reshape, filters=64, kernel_size=4, strides=2, activation=tf.nn.relu, name="d_3a", reuse=reuse, padding="same")
+            d_3b = tf.layers.conv2d_transpose(inputs=d_3a, filters=64, kernel_size=4, strides=2, activation=tf.nn.relu, name="d_3b", reuse=reuse, padding="same")
+            d_4 = tf.layers.conv2d_transpose(inputs=d_3b, filters=32, kernel_size=4, strides=2, activation=tf.nn.relu, name="d_4", reuse=reuse, padding="same")
+            d_out = tf.layers.conv2d_transpose(inputs=d_4, filters=1, kernel_size=4, strides=2, name="d_out", reuse=reuse, padding="same")
+            # In the FactorVAE paper, they say (Table 1) that they only use 3 "upconv" layers.
+            # My code above deviates from that because I was getting d_out.shape=[?, 46, 46, 1]
+            # with only 3 layers and default padding="valid"
         return d_out
+
+    def _discriminator_init(self, inputs, reuse=False):
+        with tf.variable_scope("discriminator"):
+            disc_1 = tf.layers.dense(inputs=inputs, units=1000, activation=tf.nn.leaky_relu, name="disc_1", reuse=reuse)
+            disc_2 = tf.layers.dense(inputs=disc_1, units=1000, activation=tf.nn.leaky_relu, name="disc_2", reuse=reuse)
+            disc_3 = tf.layers.dense(inputs=disc_2, units=1000, activation=tf.nn.leaky_relu, name="disc_3", reuse=reuse)
+            disc_4 = tf.layers.dense(inputs=disc_3, units=1000, activation=tf.nn.leaky_relu, name="disc_4", reuse=reuse)
+            disc_5 = tf.layers.dense(inputs=disc_4, units=1000, activation=tf.nn.leaky_relu, name="disc_5", reuse=reuse)
+            disc_6 = tf.layers.dense(inputs=disc_5, units=1000, activation=tf.nn.leaky_relu, name="disc_6", reuse=reuse)
+
+            logits = tf.layers.dense(inputs=disc_6, units=2, name="disc_logits", reuse=reuse)
+            probabilities = tf.nn.softmax(logits)
+
+        return logits, probabilities
 
     def _loss_init(self):
         ### Regulariser part of loss has two parts: KL divergence and Total Correlation
@@ -109,9 +142,8 @@ class FactorVAE(object):
         permuted_samples = tf.stack(permuted_rows, axis=1)
 
         # define discriminator network to distinguish between real and permuted q(z)
-        with tf.variable_scope("discriminator"):
-            logits_real, probs_real = _discriminator_init(real_samples)
-            logits_permuted, probs_permuted = _discriminator_init(permuted_samples, reuse=True)
+        logits_real, probs_real = self._discriminator_init(real_samples)
+        logits_permuted, probs_permuted = self._discriminator_init(permuted_samples, reuse=True)
 
         # FactorVAE paper has gamma * log(D(z) / (1- D(z))) in Algorithm 2, where D(z) is probability of being real
         # Let PT be probability of being true, PF be probability of being false. Then we want log(PT/PF)
@@ -140,17 +172,6 @@ class FactorVAE(object):
 
         return recon_loss, auto_encoder_loss, disc_loss
 
-    def _discriminator_init(self, inputs, reuse=False):
-        disc_1 = tf.layers.dense(inputs=inputs, units=1000, activation=tf.nn.leaky_relu, name="disc_1", reuse=reuse)
-        disc_2 = tf.layers.dense(inputs=disc_1, units=1000, activation=tf.nn.leaky_relu, name="disc_2", reuse=reuse)
-        disc_3 = tf.layers.dense(inputs=disc_2, units=1000, activation=tf.nn.leaky_relu, name="disc_3", reuse=reuse)
-        disc_4 = tf.layers.dense(inputs=disc_3, units=1000, activation=tf.nn.leaky_relu, name="disc_4", reuse=reuse)
-        disc_5 = tf.layers.dense(inputs=disc_4, units=1000, activation=tf.nn.leaky_relu, name="disc_5", reuse=reuse)
-        disc_6 = tf.layers.dense(inputs=disc_5, units=1000, activation=tf.nn.leaky_relu, name="disc_6", reuse=reuse)
-
-        logits = tf.layers.dense(inputs=disc_6, units=2, name="disc_logits", reuse=reuse)
-        probabilities = tf.nn.softmax(logits)
-        return logits, probabilities
 
     def _optimizer_init(self):
         enc_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='encoder')
@@ -161,3 +182,12 @@ class FactorVAE(object):
         disc_train_step = tf.train.AdamOptimizer(learning_rate=10e-4, beta1=0.5, beta2=0.9).minimize(-self.disc_loss, var_list=disc_vars)
 
         return ae_train_step, disc_train_step
+
+
+if __name__ == "__main__":
+    mode = sys.argv[1]
+    vae = FactorVAE()
+    if mode == "train":
+        vae.train()
+    elif mode == "load":
+        vae.load_latest_checkpoint()
